f36d2bab14906d95f42696868b1a6e070b463e29
Bakatkin <bakatkin@gmail.com>
2020-09-15 12:41:42 +0300

    Реализована задача 1.1.3. Тесты пройдены успешно.
    Исправлены замечания по задаче.

--- src/main/java/util/Util.java
+++ src/main/java/util/Util.java
@@ -6,16 +6,9 @@ import java.sql.SQLException;
 
 public class Util {
     public static Connection getSQLConnection() throws SQLException, ClassNotFoundException {
-        final String HOSTNAME = "localhost";
-        final String DBNAME = "tasks_db";
-        final String USERNAME = "root";
-        final String PASSWORD = "svpunk";
-        return getSQLConnection(HOSTNAME, DBNAME, USERNAME, PASSWORD);
-    }
-
-    private static Connection getSQLConnection(String hostname, String dbname, String username, String password) throws ClassNotFoundException, SQLException {
-        String connectionURL = "jdbc:mysql://" + hostname + ":3306/" + dbname + "?serverTimezone=Europe/Moscow&useSSL=false";
-        Connection conn = DriverManager.getConnection(connectionURL, username, password);
-        return conn;
+        return DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/tasks_db?serverTimezone=Europe/Moscow&useSSL=false",
+                "root",
+                "svpunk");
     }
 }
