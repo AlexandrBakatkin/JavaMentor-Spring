d08f30089dd94d210606b906187b70056752c8ab
Bakatkin <bakatkin@gmail.com>
2020-10-29 10:53:37 +0300

    Решение задачи 2.1.4. Тесты пройдены.
    Решение исправлено.

--- src/main/java/koschei/config/AppConfig.java
+++ src/main/java/koschei/config/AppConfig.java
@@ -1,7 +1,6 @@
 package koschei.config;
 
-import koschei.models.Island2;
-import koschei.models.Wood3;
+import koschei.models.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -15,4 +14,8 @@ public class AppConfig {
         return new Island2(wood);
     }
 
+    @Bean
+    public static Duck5 getDuck (Egg6 egg6){
+        return new Duck5(egg6);
+    }
 }
--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -5,8 +5,12 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Egg6 {
-    @Autowired
     private Needle7 needle7;
+
+    @Autowired
+    public void setNeedle7(Needle7 needle7) {
+        this.needle7 = needle7;
+    }
 
     @Override
     public String toString() {
